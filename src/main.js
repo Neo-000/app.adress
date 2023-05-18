0d7c378d86acef58ec14c57ffee64a068d4ad3c3
Neo-000 <nekrylovvova@mail.ru>
2023-05-19 00:20:07 +0300

    #1

---
+++ src/main.js
@@ -0,0 +1,111 @@
+const input = document.getElementById('input');
+const switches = [];
+const value = {
+    number:'12',
+    active:{
+        item_1:false,
+        item_2:false,
+        item_3:false,
+        item_4:false,
+        item_5:false,
+        item_6:false,
+        item_7:false,
+        item_8:false
+    },
+    enums:[]
+
+}
+const chekpoint ={
+    item_1:1,
+    item_2:2,
+    item_3:4,
+    item_4:8,
+    item_5:16,
+    item_6:32,
+    item_7:64,
+    item_8:128
+};
+
+function logik(){
+    console.log('!')
+    let summ = Number(value.number);
+    for(let i = 1; i < switches.length;i++){
+        for(let items in chekpoint){
+            if(switches[i] >= summ){
+                summ = Number(summ) - Number(chekpoint[items]);
+                switches[i]
+            }
+        }
+    }
+}
+
+
+
+function switched(){
+    let summ = 0;
+    for(let key in chekpoint){
+        if(value.active[key] == true){
+            summ = Number(summ) + Number(chekpoint[key])
+        }
+    }
+    value.number = Number(summ)
+    PushNumber();
+}
+
+function event_switches(){
+    for(let i = 1;i < switches.length;i++){
+        switches[i].onclick = () =>{
+            let id = `item_` + i;
+            if(switches[i].checked){
+             for (let key in value.active) {
+                    if(key == id){
+                        value.active[key] = true
+                    }
+                }
+            }else{
+                for (let key in value.active) {
+                    if(key == id){
+                        value.active[key] = false
+                    }
+                }
+            }
+            switched()
+            // console.log(value.active)
+          }
+        }
+    }
+
+function getSwitches(){
+    for(let i = 1; i < 9; i++){
+        switches[i] = document.getElementById(`item_${i}`)
+    }
+}
+
+function PushNumber(){
+    input.value = value.number
+}
+
+
+
+function init(){
+    getSwitches();
+    event_switches();
+    input.oninput = () => {
+        // console.log('input invaled')
+        value.number = Number(input.value);
+        if(Number(value.number) > 225){
+            input.value = 225
+            value.number = Number(input.value)
+        }
+        if(Number(input.value) < 0){
+            input.value = 0
+        }
+        logik()
+        // console.log(value.number)
+    }
+    // console.log(switches)
+}
+
+
+
+init()
